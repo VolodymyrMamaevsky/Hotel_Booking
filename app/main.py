887d572f73adc26463761d6ca9b9a1f55acb0e2a
VolodymyrMamaevsky <vova.moder@gmail.com>
2024-01-09 17:33:27 +0200

    add 2 endpoints & validations

--- app/main.py
+++ app/main.py
@@ -1,8 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Depends
+from typing import Optional
+from datetime import date
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class HotelSearchArgs:
+    def __init__(
+        self,
+        location,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+    ):
+        self.location = location
+        self.date_from = date_from
+        self.date_to = date_to
+        self.has_spa = has_spa
+        self.stars = stars
+
+
+class SHotel(BaseModel):
+    adress: str
+    name: str
+    stars: int
+
+
 @app.get("/hotels")
-def get_hotels():
-    return "Hotel"
+def get_hotels(search_args: HotelSearchArgs = Depends()):
+    return search_args
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/booking")
+def add_booking(booking: SBooking):
+    pass
